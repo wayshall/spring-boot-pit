16a013fb199ee0feab0d831593a6d7509c448abd
wayshall <weishao.zeng@gmail.com>
2015-06-24 22:36:53 +0800

    add args constructor

--- src/test/java/org/onetwo/boot/core/web/ftl/FreemarkerTaglibsConfigTest.java
+++ src/test/java/org/onetwo/boot/core/web/ftl/FreemarkerTaglibsConfigTest.java
@@ -1,12 +1,15 @@
 package org.onetwo.boot.core.web.ftl;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FreemarkerTaglibsConfigTest {
 
+
 	@Bean
+	@ConditionalOnMissingBean(ClassPathTldsLoader.class)
 	public ClassPathTldsLoader classPathTldsLoader(){
 		return new ClassPathTldsLoader();
 	}
